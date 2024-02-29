f46671e73a53236cb54924a1b370edf86baf2dfd
Octavian Pashya Ramadhan <107595390+Pashyaaaa@users.noreply.github.com>
2024-02-29 07:27:53 +0700

    Mengubah src bootstrap

--- petugas/footer.php
+++ petugas/footer.php
@@ -7,6 +7,6 @@
             </div>
         </div>
     </div>
-    <script src="../assets/bootstrap-5.3.2/dist/js/bootstrap.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
