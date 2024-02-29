b19cdb1ca2798de5bcd4b7170bf86a54adedb579
Octavian Pashya Ramadhan <107595390+Pashyaaaa@users.noreply.github.com>
2024-02-29 07:28:18 +0700

    Mengubah src bootstrap

--- petugas/header.php
+++ petugas/header.php
@@ -14,7 +14,7 @@ if($_SESSION['level'] != '2'){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Petugas</title>
-    <link rel="stylesheet" href="../assets/bootstrap-5.3.2/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <style>
     .btn-biru-tua {
